3060884492acc20652b57206c1f345567c811e69
Farid Boulos <fjb04@mail.aub.edu>
2022-09-29 21:48:34 +0300

    Completed project_pathplan/DFS implementation

--- project_pathplan/graph_search.js
+++ project_pathplan/graph_search.js
@@ -24,55 +24,170 @@
 \/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/*/
 
 function initSearchGraph() {
+  // create the search queue
+  visit_queue = [];
+  // initialize search graph as 2D array over configuration space
+  //   of 2D locations with specified spatial resolution
+  G = [];
+  for (iind = 0, xpos = -2; xpos < 7; iind++, xpos += eps) {
+    G[iind] = [];
+    for (jind = 0, ypos = -2; ypos < 7; jind++, ypos += eps) {
+      G[iind][jind] = {
+        i: iind,
+        j: jind, // mapping to graph array
+        x: xpos,
+        y: ypos, // mapping to map coordinates
+        parent: null, // pointer to parent in graph along motion path
+        distance: 10000, // distance to start via path through parent
+        visited: false, // flag for whether the node has been visited
+        priority: null, // visit priority based on fscore
+        queued: false, // flag for whether the node has been queued for visiting
+      };
 
-    // create the search queue
-    visit_queue = [];
-
-    // initialize search graph as 2D array over configuration space
-    //   of 2D locations with specified spatial resolution
-    G = [];
-    for (iind=0,xpos=-2;xpos<7;iind++,xpos+=eps) {
-        G[iind] = [];
-        for (jind=0,ypos=-2;ypos<7;jind++,ypos+=eps) {
-            G[iind][jind] = {
-                i:iind,j:jind, // mapping to graph array
-                x:xpos,y:ypos, // mapping to map coordinates
-                parent:null, // pointer to parent in graph along motion path
-                distance:10000, // distance to start via path through parent
-                visited:false, // flag for whether the node has been visited
-                priority:null, // visit priority based on fscore
-                queued:false // flag for whether the node has been queued for visiting
-            };
-
-            // STENCIL: determine whether this graph node should be the start
-            //   point for the search
-        }
+      // STENCIL: determine whether this graph node should be the start
+      //   point for the search
+      if (xpos < 0.01 && xpos > -0.01 && ypos < 0.01 && ypos > -0.01) {
+        G[iind][jind].distance = 0;
+        G[iind][jind].visited = true;
+        visit_queue.push(G[iind][jind]);
+      }
     }
+  }
 }
 
 function iterateGraphSearch() {
+  // STENCIL: implement a single iteration of a graph search algorithm
+  //   for A-star (or DFS, BFS, Greedy Best-First)
+  //   An asynch timing mechanism is used instead of a for loop to avoid
+  //   blocking and non-responsiveness in the browser.
+  //
+  //   Return "failed" if the search fails on this iteration.
+  //   Return "succeeded" if the search succeeds on this iteration.
+  //   Return "iterating" otherwise.
+  //
+  //   Provided support functions:
+  //
+  //   testCollision - returns whether a given configuration is in collision
+  //   drawHighlightedPathGraph - draws a path back to the start location
+  //   draw_2D_configuration - draws a square at a given location
 
+  current_node = visit_queue.pop();
 
-    // STENCIL: implement a single iteration of a graph search algorithm
-    //   for A-star (or DFS, BFS, Greedy Best-First)
-    //   An asynch timing mechanism is used instead of a for loop to avoid
-    //   blocking and non-responsiveness in the browser.
-    //
-    //   Return "failed" if the search fails on this iteration.
-    //   Return "succeeded" if the search succeeds on this iteration.
-    //   Return "iterating" otherwise.
-    //
-    //   Provided support functions:
-    //
-    //   testCollision - returns whether a given configuration is in collision
-    //   drawHighlightedPathGraph - draws a path back to the start location
-    //   draw_2D_configuration - draws a square at a given location
+  console.log(
+    testCollision([
+      G[current_node.i][current_node.j].x,
+      G[current_node.i][current_node.j].y,
+    ]),
+    G[current_node.i][current_node.j]
+  );
+  G[current_node.i][current_node.j].visited = true;
+
+  adjacent = [];
+
+  if (
+    !G[current_node.i - 1][current_node.j].queued &&
+    !testCollision([
+      G[current_node.i - 1][current_node.j].x,
+      G[current_node.i - 1][current_node.j].y,
+    ])
+  ) {
+    visit_queue.push(G[current_node.i - 1][current_node.j]);
+
+    G[current_node.i - 1][current_node.j].queued = true;
+    if (
+      G[current_node.i - 1][current_node.j].distance >
+      G[current_node.i][current_node.j].distance + eps
+    ) {
+      G[current_node.i - 1][current_node.j].parent =
+        G[current_node.i][current_node.j];
+      G[current_node.i - 1][current_node.j].distance =
+        G[current_node.i][current_node.j].distance + eps;
+      G[current_node.i - 1][current_node.j].visited = true;
+    }
+  }
+  if (
+    !G[current_node.i][current_node.j - 1].queued &&
+    !testCollision([
+      G[current_node.i][current_node.j - 1].x,
+      G[current_node.i][current_node.j - 1].y,
+    ])
+  ) {
+    visit_queue.push(G[current_node.i][current_node.j - 1]);
+
+    G[current_node.i][current_node.j - 1].queued = true;
+    if (
+      G[current_node.i][current_node.j - 1].distance >
+      G[current_node.i][current_node.j].distance + eps
+    ) {
+      G[current_node.i][current_node.j - 1].parent =
+        G[current_node.i][current_node.j];
+      G[current_node.i][current_node.j - 1].distance =
+        G[current_node.i][current_node.j].distance + eps;
+      G[current_node.i][current_node.j - 1].visited = true;
+    }
+  }
+  if (
+    !G[current_node.i + 1][current_node.j].queued &&
+    !testCollision([
+      G[current_node.i + 1][current_node.j].x,
+      G[current_node.i + 1][current_node.j].y,
+    ])
+  ) {
+    visit_queue.push(G[current_node.i + 1][current_node.j]);
+    G[current_node.i + 1][current_node.j].queued = true;
+    if (
+      G[current_node.i + 1][current_node.j].distance >
+      G[current_node.i][current_node.j].distance + eps
+    ) {
+      G[current_node.i + 1][current_node.j].parent =
+        G[current_node.i][current_node.j];
+      G[current_node.i + 1][current_node.j].distance =
+        G[current_node.i][current_node.j].distance + eps;
+      G[current_node.i + 1][current_node.j].visited = true;
+    }
+  }
+  if (
+    !G[current_node.i][current_node.j + 1].queued &&
+    !testCollision([
+      G[current_node.i][current_node.j + 1].x,
+      G[current_node.i][current_node.j + 1].y,
+    ])
+  ) {
+    visit_queue.push(G[current_node.i][current_node.j + 1]);
+    G[current_node.i][current_node.j + 1].queued = true;
+    if (
+      G[current_node.i][current_node.j + 1].distance >
+      G[current_node.i][current_node.j].distance + eps
+    ) {
+      G[current_node.i][current_node.j + 1].parent =
+        G[current_node.i][current_node.j];
+      G[current_node.i][current_node.j + 1].distance =
+        G[current_node.i][current_node.j].distance + eps;
+      G[current_node.i][current_node.j + 1].visited = true;
+    }
+  }
+
+  if (
+    testCollision([
+      G[current_node.i][current_node.j].x,
+      G[current_node.i][current_node.j].y,
+    ])
+  ) {
+    drawHighlightedPathGraph(current_node);
+    search_iterate = false;
+    return "failed";
+  } else if (G[current_node.i][current_node.j] == G[60][60]) {
+    drawHighlightedPathGraph(current_node);
+    search_iterate = false;
+    return "succeeded";
+  } else {
+    return "iterating";
+  }
 }
 
 //////////////////////////////////////////////////
 /////     MIN HEAP IMPLEMENTATION FUNCTIONS
 //////////////////////////////////////////////////
 
-    // STENCIL: implement min heap functions for graph search priority queue.
-    //   These functions work use the 'priority' field for elements in graph.
-
+// STENCIL: implement min heap functions for graph search priority queue.
+//   These functions work use the 'priority' field for elements in graph.
--- project_pathplan/infrastructure.js
+++ project_pathplan/infrastructure.js
@@ -37,73 +37,68 @@
 //////////////////////////////////////////////////
 
 function testCollision(q) {
+  var j;
 
-    var j;
+  // test for collision with each object
+  for (j = 0; j < range.length; j++) {
+    // assume configuration is in collision
+    var in_collision = true;
 
-    // test for collision with each object
-    for (j=0;j<range.length;j++) {
-
-        // assume configuration is in collision
-        var in_collision = true;
-
-        // no collision detected, if configuration is outside obstacle along any dimension
-        for (i=0;i<q.length;i++) {
-            if ((q[i]<range[j][i][0])||(q[i]>range[j][i][1]))
-                in_collision = false;
-        }
-
-        // return collision, if configuration inside obstacle extents along all dimensions
-        if (in_collision)
-            return true;
+    // no collision detected, if configuration is outside obstacle along any dimension
+    for (i = 0; i < q.length; i++) {
+      if (q[i] < range[j][i][0] || q[i] > range[j][i][1]) in_collision = false;
     }
 
-    // return no collision, if no collision detected with any obstacle
-    return false;
+    // return collision, if configuration inside obstacle extents along all dimensions
+    if (in_collision) return true;
+  }
+
+  // return no collision, if no collision detected with any obstacle
+  return false;
 }
 
 function initRRT(q) {
+  // create tree object
+  var tree = {};
 
-    // create tree object
-    var tree = {};
+  // initialize with vertex for given configuration
+  tree.vertices = [];
+  tree.vertices[0] = {};
+  tree.vertices[0].vertex = q;
+  tree.vertices[0].edges = [];
 
-    // initialize with vertex for given configuration
-    tree.vertices = [];
-    tree.vertices[0] = {};
-    tree.vertices[0].vertex = q;
-    tree.vertices[0].edges = [];
+  // maintain index of newest vertex added to tree
+  tree.newest = 0;
 
-    // maintain index of newest vertex added to tree
-    tree.newest = 0;
-
-    return tree;
+  return tree;
 }
 
-function insertTreeVertex(tree,q) {
+function insertTreeVertex(tree, q) {
+  // create new vertex object for tree with given configuration and no edges
+  new_vertex = {};
+  new_vertex.edges = [];
+  new_vertex.vertex = q;
+  tree.vertices.push(new_vertex);
 
-    // create new vertex object for tree with given configuration and no edges
-    new_vertex = {};
-    new_vertex.edges = [];
-    new_vertex.vertex = q;
-    tree.vertices.push(new_vertex);
+  // maintain index of newest vertex added to tree
+  tree.newest = tree.vertices.length - 1;
 
-    // maintain index of newest vertex added to tree
-    tree.newest = tree.vertices.length - 1;
-
-    // draw location on canvas
-    draw_2D_configuration(q, "visited");
+  // draw location on canvas
+  draw_2D_configuration(q, "visited");
 }
 
+function insertTreeEdge(tree, q1_idx, q2_idx) {
+  // add edge to first vertex as pointer to second vertex
+  tree.vertices[q1_idx].edges.push(tree.vertices[q2_idx]);
 
-function insertTreeEdge(tree,q1_idx,q2_idx) {
+  // add edge to second vertex as pointer to first vertex
+  tree.vertices[q2_idx].edges.push(tree.vertices[q1_idx]);
 
-    // add edge to first vertex as pointer to second vertex
-    tree.vertices[q1_idx].edges.push(tree.vertices[q2_idx]);
-
-    // add edge to second vertex as pointer to first vertex
-    tree.vertices[q2_idx].edges.push(tree.vertices[q1_idx]);
-
-    // draw edge on canvas
-    draw_2D_edge_configurations(tree.vertices[q1_idx].vertex,tree.vertices[q2_idx].vertex);
+  // draw edge on canvas
+  draw_2D_edge_configurations(
+    tree.vertices[q1_idx].vertex,
+    tree.vertices[q2_idx].vertex
+  );
 }
 
 //////////////////////////////////////////////////
@@ -116,59 +111,111 @@ function insertTreeEdge(tree,q1_idx,q2_idx) {
 //       (relative to the PAGE, not the canvas) into a world coordinate
 
 function xformWorldViewX(world_x) {
-    return (world_x*100)+200;  // view_x
+  return world_x * 100 + 200; // view_x
 }
 function xformWorldViewY(world_y) {
-    return (world_y*100)+200;  // view_y
+  return world_y * 100 + 200; // view_y
 }
 function xformMouseWorldX(view_x) {
-    return (view_x-canvas_side_off-200)/100;  // world_x
+  return (view_x - canvas_side_off - 200) / 100; // world_x
 }
 function xformMouseWorldY(view_y) {
-    return (view_y-canvas_top_off-200)/100;  // world_y
+  return (view_y - canvas_top_off - 200) / 100; // world_y
 }
 
 function setPlanningScene() {
+  // obstacles specified as a range along [0] (x-dimension) and [1] y-dimension
+  range = []; // global variable
 
-    // obstacles specified as a range along [0] (x-dimension) and [1] y-dimension
-    range = []; // global variable
+  // world boundary
+  range[0] = [
+    [-1.8, 5.8],
+    [-1.8, -1],
+  ];
+  range[1] = [
+    [-1.8, 5.8],
+    [5, 5.8],
+  ];
+  range[2] = [
+    [-1.8, -1],
+    [-1.8, 5.8],
+  ];
+  range[3] = [
+    [5, 5.8],
+    [-1.8, 5.8],
+  ];
 
-    // world boundary
-    range[0] = [ [-1.8,5.8],[-1.8,-1] ];
-    range[1] = [ [-1.8,5.8],[5,5.8] ];
-    range[2] = [ [-1.8,-1], [-1.8,5.8] ];
-    range[3] = [ [5,5.8],   [-1.8,5.8] ];
+  if (typeof planning_scene === "undefined") planning_scene = "empty";
 
-    if (typeof planning_scene === 'undefined')
-        planning_scene = 'empty';
-
-    if (planning_scene == 'misc') {
-        /*  misc stuff with narrow opening */
-        range[4] = [ [1,2],[1,2] ];
-        range[5] = [ [3,3.3],[1,4] ];
-        range[6] = [ [0.6,0.7],[0.4,0.7] ];
-        range[7] = [ [3.7,3.9],[-0.8,5] ];
-    }
-    else if (planning_scene == 'narrow1') {
-        /*  narrow path 1 */
-        range[4] = [ [1,3],[4,5] ];
-        range[5] = [ [1,3],[-1,2] ];
-        range[6] = [ [1,1.95],[2,3.8] ];
-    }
-    else if (planning_scene == 'narrow2') {
-        /*  narrow path 2 */
-        range[4] = [ [1,3],[4,5] ];
-        range[5] = [ [1,3],[-1,2] ];
-        range[6] = [ [1,1.9],[2,3.8] ];
-        range[7] = [ [2.1,3],[2.2,4] ];
-    }
-    else if (planning_scene == 'three_sections') {
-        /*  three compartments */
-        range[4] = [ [1,1.3],[4,5] ];
-        range[5] = [ [1,1.3],[-1,3.5] ];
-        range[6] = [ [2.7,3],[-1,0] ];
-        range[7] = [ [2.7,3],[.5,5] ];
-    }
+  if (planning_scene == "misc") {
+    /*  misc stuff with narrow opening */
+    range[4] = [
+      [1, 2],
+      [1, 2],
+    ];
+    range[5] = [
+      [3, 3.3],
+      [1, 4],
+    ];
+    range[6] = [
+      [0.6, 0.7],
+      [0.4, 0.7],
+    ];
+    range[7] = [
+      [3.7, 3.9],
+      [-0.8, 5],
+    ];
+  } else if (planning_scene == "narrow1") {
+    /*  narrow path 1 */
+    range[4] = [
+      [1, 3],
+      [4, 5],
+    ];
+    range[5] = [
+      [1, 3],
+      [-1, 2],
+    ];
+    range[6] = [
+      [1, 1.95],
+      [2, 3.8],
+    ];
+  } else if (planning_scene == "narrow2") {
+    /*  narrow path 2 */
+    range[4] = [
+      [1, 3],
+      [4, 5],
+    ];
+    range[5] = [
+      [1, 3],
+      [-1, 2],
+    ];
+    range[6] = [
+      [1, 1.9],
+      [2, 3.8],
+    ];
+    range[7] = [
+      [2.1, 3],
+      [2.2, 4],
+    ];
+  } else if (planning_scene == "three_sections") {
+    /*  three compartments */
+    range[4] = [
+      [1, 1.3],
+      [4, 5],
+    ];
+    range[5] = [
+      [1, 1.3],
+      [-1, 3.5],
+    ];
+    range[6] = [
+      [2.7, 3],
+      [-1, 0],
+    ];
+    range[7] = [
+      [2.7, 3],
+      [0.5, 5],
+    ];
+  }
 }
 
 //////////////////////////////////////////////////
@@ -176,174 +223,174 @@ function setPlanningScene() {
 //////////////////////////////////////////////////
 
 function init() {
-    // store the location of the canvas relative to the webpage
-    var b_rect = document.getElementById("myCanvas").getBoundingClientRect();
-    canvas_side_off = b_rect.left;
-    canvas_top_off = b_rect.top;
+  // store the location of the canvas relative to the webpage
+  var b_rect = document.getElementById("myCanvas").getBoundingClientRect();
+  canvas_side_off = b_rect.left;
+  canvas_top_off = b_rect.top;
 
-    // initialize search variables, data structures, DOM elements, etc.
-    initSearch();
+  // initialize search variables, data structures, DOM elements, etc.
+  initSearch();
 
-    // start main animation/iteration loop
-    animate();
+  // start main animation/iteration loop
+  animate();
 }
 
 function initSearch() {
+  // specify default search algorithm to use for planning
+  //search_alg = "depth-first";
+  //search_alg = "breadth-first";
+  //search_alg = "greedy-best-first";
+  search_alg = "A-star";
+  //search_alg = "RRT";
+  //search_alg = "RRT-connect";
+  //search_alg = "RRT-star";
 
-    // specify default search algorithm to use for planning
-    //search_alg = "depth-first";
-    //search_alg = "breadth-first";
-    //search_alg = "greedy-best-first";
-    search_alg = "A-star";
-    //search_alg = "RRT";
-    //search_alg = "RRT-connect";
-    //search_alg = "RRT-star";
+  // specify default the world for the planner
+  //  (stored as "range" global variable with name "planning_scene")
+  //planning_scene = "empty";
+  planning_scene = "misc";
+  //planning_scene = "narrow1";
+  //planning_scene = "narrow2";
+  //planning_scene = "three_sections";
 
-    // specify default the world for the planner
-    //  (stored as "range" global variable with name "planning_scene")
-    //planning_scene = "empty";
-    planning_scene = "misc";
-    //planning_scene = "narrow1";
-    //planning_scene = "narrow2";
-    //planning_scene = "three_sections";
+  // specify default eps (epsilon) spatial resolution variable
+  //   for RRT, specifies threshold radius for step size and reaching goal
+  eps = 0.1;
 
-    // specify default eps (epsilon) spatial resolution variable
-    //   for RRT, specifies threshold radius for step size and reaching goal
-    eps = 0.1;
+  // create event handlers for the mouse
+  canvas = document.getElementById("myCanvas");
+  mouse_x = 0;
+  mouse_y = 0;
 
-    // create event handlers for the mouse
-    canvas = document.getElementById("myCanvas");
-    mouse_x = 0;
-    mouse_y = 0;
+  // when the mouse moves, update the mouse's location
+  canvas.onmousemove = function handleMouseMove(event) {
+    mouse_x = event.clientX;
+    mouse_y = event.clientY;
+  };
 
-    // when the mouse moves, update the mouse's location
-    canvas.onmousemove = function handleMouseMove(event) {
-        mouse_x = event.clientX;
-        mouse_y = event.clientY;
-    };
+  // when the mouse button is pressed, update mouseDown
+  canvas.onmousedown = function () {
+    mouseDown = 1;
+  };
 
-    // when the mouse button is pressed, update mouseDown
-    canvas.onmousedown = function() {
-        mouseDown = 1;
-    };
+  // when the mouse button is released, update mouseDown
+  canvas.onmouseup = function () {
+    mouseDown = 0;
+    q_goal = [xformMouseWorldX(mouse_x), xformMouseWorldY(mouse_y)];
+    restartSearch();
+  };
 
-    // when the mouse button is released, update mouseDown
-    canvas.onmouseup = function() {
-        mouseDown = 0;
-        q_goal = [xformMouseWorldX(mouse_x),xformMouseWorldY(mouse_y)];
-        restartSearch();
-    };
+  // specify start and goal configurations
+  q_start_config = [0, 0];
+  q_goal_config = [4, 4];
+  q_init = q_start_config;
+  q_goal = q_goal_config;
 
-    // specify start and goal configurations
-    q_start_config = [0,0];
-    q_goal_config = [4,4];
-    q_init = q_start_config;
-    q_goal = q_goal_config;
+  // keep track of the last goal drawn on the canvas
+  prev_q_goal = [10000, 1000];
 
-    // keep track of the last goal drawn on the canvas
-    prev_q_goal = [10000,1000];
+  color_scheme = "default";
 
-    color_scheme = "default";
+  var url_parsed = window.location.href.split("?");
+  for (i = 1; i < url_parsed.length; i++) {
+    var param_parsed = url_parsed[i].split("=");
+    //eval(param_parsed[0]+"=\'"+param_parsed[1]+"\'");
+    if (
+      param_parsed[0] !== "search_alg" &&
+      param_parsed[0] !== "planning_scene" &&
+      param_parsed[0] !== "color_scheme"
+    )
+      eval(param_parsed[0] + "=" + param_parsed[1]);
+    else eval(param_parsed[0] + "='" + param_parsed[1] + "'");
+  }
 
-    var url_parsed = window.location.href.split("?");
-    for (i=1;i<url_parsed.length;i++) {
-        var param_parsed = url_parsed[i].split("=");
-        //eval(param_parsed[0]+"=\'"+param_parsed[1]+"\'");
-        if ((param_parsed[0] !== "search_alg")&&(param_parsed[0] !== "planning_scene")&&(param_parsed[0] !== "color_scheme"))
-            eval(param_parsed[0]+"="+param_parsed[1]);
-        else
-            eval(param_parsed[0]+"=\'"+param_parsed[1]+"\'");
-    }
+  // set up the color constants
+  initColorScheme(color_scheme);
 
+  // set the world for the planner \
+  setPlanningScene();
 
-    // set up the color constants
-    initColorScheme(color_scheme);
+  // initialize search tree from start configurations (RRT-based algorithms)
+  T_a = initRRT(q_init);
+  // also initialize search tree from goal configuration (RRT-Connect)
+  T_b = initRRT(q_goal);
 
-    // set the world for the planner \
-    setPlanningScene();
+  // initialize graph search algorithms (DFS, BFS, A-star)
+  initSearchGraph();
 
-    // initialize search tree from start configurations (RRT-based algorithms)
-    T_a = initRRT(q_init);
-    // also initialize search tree from goal configuration (RRT-Connect)
-    T_b = initRRT(q_goal);
+  // flag to continue or stop search iterations
+  search_iterate = true;
 
-    // initialize graph search algorithms (DFS, BFS, A-star)
-    initSearchGraph();
+  // counter for number of search iterations executed
+  search_iter_count = 0;
+  search_result = "starting";
 
-    // flag to continue or stop search iterations
-    search_iterate = true;
+  // threshold for number of maximum search iterations for certain algorithms
+  search_max_iterations = 10000;
 
-    // counter for number of search iterations executed
-    search_iter_count = 0;
-    search_result = "starting";
+  // counter for number of configurations visited
+  search_visited = 0;
 
-    // threshold for number of maximum search iterations for certain algorithms
-    search_max_iterations = 10000;
+  // variable to sum final path length
+  path_length = 0;
 
-    // counter for number of configurations visited
-    search_visited = 0;
+  // don't draw the path until asked to
+  draw_path = false;
 
-    // variable to sum final path length
-    path_length = 0;
+  // capture the current system time for timing of successive iterations
+  //   using the given Date object
+  cur_time = Date.now();
 
-    // don't draw the path until asked to
-    draw_path = false;
+  // specify minimum number of milliseconds between successive search
+  //   iterations
+  min_msec_between_iterations = 20;
 
-    // capture the current system time for timing of successive iterations
-    //   using the given Date object
-    cur_time = Date.now();
-
-    // specify minimum number of milliseconds between successive search
-    //   iterations
-    min_msec_between_iterations = 20;
-
-    // create textbar DOM element for text output to browser window
-    textbar = document.createElement('div');
-    textbar.style.zIndex = 0;    // if you still don't see the label, try uncommenting this
-    textbar.style.position = 'absolute';
-    textbar.style.width = window.width-10;
-    textbar.style["font-family"] = "Monospace";
-    textbar.style["font-size"] = "12px";
-    textbar.style.height = 20;
-    if (color_scheme == "light") {
-        textbar.style.color = "#FFFFFF";
-    } else {
-        textbar.style.color = "#000000";
-    }
-    textbar.innerHTML = "Search Canvas";
-    //textbar.style.top = 30 + 'px';  // position textbar wrt. document
-    textbar.style.top = (23 + document.getElementById("myCanvas").offsetTop) + 'px';  // position textbar wrt. canvas
-    textbar.style.left = (25 + document.getElementById("myCanvas").offsetLeft) + 'px';  // position textbar wrt. canvas
-    document.body.appendChild(textbar);
-
+  // create textbar DOM element for text output to browser window
+  textbar = document.createElement("div");
+  textbar.style.zIndex = 0; // if you still don't see the label, try uncommenting this
+  textbar.style.position = "absolute";
+  textbar.style.width = window.width - 10;
+  textbar.style["font-family"] = "Monospace";
+  textbar.style["font-size"] = "12px";
+  textbar.style.height = 20;
+  if (color_scheme == "light") {
+    textbar.style.color = "#FFFFFF";
+  } else {
+    textbar.style.color = "#000000";
+  }
+  textbar.innerHTML = "Search Canvas";
+  //textbar.style.top = 30 + 'px';  // position textbar wrt. document
+  textbar.style.top = 23 + document.getElementById("myCanvas").offsetTop + "px"; // position textbar wrt. canvas
+  textbar.style.left =
+    25 + document.getElementById("myCanvas").offsetLeft + "px"; // position textbar wrt. canvas
+  document.body.appendChild(textbar);
 }
 
 // reset counters and the search data structure without re-initializing the whole
 // canvas environment (for use when the goal is changed via mouse press)
 function restartSearch() {
+  // turn search iteration back on if it was off
+  search_iterate = true;
 
-    // turn search iteration back on if it was off
-    search_iterate = true;
+  // back to iteration zero
+  search_iter_count = 0;
 
-    // back to iteration zero
-    search_iter_count = 0;
+  // no results yet
+  search_result = "starting";
 
-    // no results yet
-    search_result = "starting";
+  // reset the count of visited states
+  search_visited = 0;
 
-    // reset the count of visited states
-    search_visited = 0;
+  // no path found yet
+  path_length = 0;
 
-    // no path found yet
-    path_length = 0;
+  // re-initialize the queue
+  initSearchGraph();
 
-    // re-initialize the queue
-    initSearchGraph();
+  // or the RRTs
+  T_a = initRRT(q_init);
+  T_b = initRRT(q_goal);
 
-    // or the RRTs
-    T_a = initRRT(q_init);
-    T_b = initRRT(q_goal);
-
-    // don't draw the path until asked to
-    draw_path = false;
+  // don't draw the path until asked to
+  draw_path = false;
 }
