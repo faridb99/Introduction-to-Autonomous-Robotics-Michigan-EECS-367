9e85515abf601d661012bce033716a1355903eac
Farid Boulos <fjb04@mail.aub.edu>
2022-10-08 17:32:40 +0300

    project_pendularm/Euler Integration- Verlet Integration -Velocity Verlet Integration Implemented

--- project_pendularm/update_pendulum_state.js
+++ project_pendularm/update_pendulum_state.js
@@ -1,60 +1,79 @@
 function update_pendulum_state(numerical_integrator, pendulum, dt, gravity) {
-    // integrate pendulum state forward in time by dt
-    // please use names 'pendulum.angle', 'pendulum.angle_previous', etc. in else codeblock between line 28-30
+  // integrate pendulum state forward in time by dt
+  // please use names 'pendulum.angle', 'pendulum.angle_previous', etc. in else codeblock between line 28-30
 
-    if (typeof numerical_integrator === "undefined")
-        numerical_integrator = "none";
+  if (typeof numerical_integrator === "undefined")
+    numerical_integrator = "none";
 
-    if (numerical_integrator === "euler") {
-
+  if (numerical_integrator === "euler") {
     // STENCIL: a correct Euler integrator is REQUIRED for assignment
-
-    }
-    else if (numerical_integrator === "verlet") {
-
+    pendulum.angle_previous = pendulum.angle;
+    pendulum.angle = pendulum.angle + pendulum.angle_dot * dt;
+    pendulum.angle_dot = pendulum.angle_dot + pendulum.angle_dot_dot * dt;
+  } else if (numerical_integrator === "verlet") {
     // STENCIL: basic Verlet integration
 
-    }
-    else if (numerical_integrator === "velocity verlet") {
+    var temp = pendulum.angle_previous;
 
+    pendulum.angle_previous = pendulum.angle;
+
+    pendulum.angle =
+      2 * pendulum.angle_previous -
+      temp +
+      pendulum.angle_dot_dot * Math.pow(dt, 2);
+  } else if (numerical_integrator === "velocity verlet") {
     // STENCIL: a correct velocity Verlet integrator is REQUIRED for assignment
 
-    }
-    else if (numerical_integrator === "runge-kutta") {
+    pendulum.angle_previous = pendulum.angle;
 
+    pendulum.angle =
+      pendulum.angle +
+      pendulum.angle_dot * dt +
+      0.5 * pendulum.angle_dot_dot * Math.pow(dt, 2);
+
+    pendulum.angle_dot =
+      pendulum.angle_dot +
+      0.5 *
+        (pendulum.angle_dot_dot + pendulum_acceleration(pendulum, gravity)) *
+        dt;
+  } else if (numerical_integrator === "runge-kutta") {
     // STENCIL: Runge-Kutta 4 integrator
-    } 
-    else {
-        pendulum.angle_previous = pendulum.angle;
-        pendulum.angle = (pendulum.angle+Math.PI/180)%(2*Math.PI);
-        pendulum.angle_dot = (pendulum.angle-pendulum.angle_previous)/dt;
-        numerical_integrator = "none";
-    }
+  } else {
+    pendulum.angle_previous = pendulum.angle;
+    pendulum.angle = (pendulum.angle + Math.PI / 180) % (2 * Math.PI);
+    pendulum.angle_dot = (pendulum.angle - pendulum.angle_previous) / dt;
+    numerical_integrator = "none";
+  }
 
-    return pendulum;
+  return pendulum;
 }
 
 function pendulum_acceleration(pendulum, gravity) {
-    // STENCIL: return acceleration(s) system equation(s) of motion 
-    
+  // STENCIL: return acceleration(s) system equation(s) of motion
+  return -(gravity / pendulum.length) * Math.sin(pendulum.angle);
 }
 
 function init_verlet_integrator(pendulum, t, gravity) {
-    // STENCIL: for verlet integration, a first step in time is needed
-    // return: updated pendulum state and time
-
-    return [pendulum, t];
+  // STENCIL: for verlet integration, a first step in time is needed
+  // return: updated pendulum state and time
+  pendulum.angle_previous = pendulum.angle;
+  pendulum.angle =
+    pendulum.angle_previous +
+    dt * pendulum.angle_dot +
+    0.5 * Math.pow(dt, 2) * pendulum.angle_dot_dot;
+  t = t + dt;
+  return [pendulum, t];
 }
 
 function set_PID_parameters(pendulum) {
-    // STENCIL: change pid parameters
-    pendulum.servo = {kp:0, kd:0, ki:0};  // no control
-    return pendulum;
+  // STENCIL: change pid parameters
+  pendulum.servo = { kp: 0, kd: 0, ki: 0 }; // no control
+  return pendulum;
 }
 
 function PID(pendulum, accumulated_error, dt) {
-    // STENCIL: implement PID controller
-    // return: updated output in pendulum.control and accumulated_error
+  // STENCIL: implement PID controller
+  // return: updated output in pendulum.control and accumulated_error
 
-    return [pendulum, accumulated_error];
+  return [pendulum, accumulated_error];
 }
